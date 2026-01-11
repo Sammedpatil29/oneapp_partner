f7e547de60f69f49f1f2b2e8642958496a07ddc1
Sammed <sammed.patil29@gmail.com>
2026-01-11 13:45:16 +0530

    oneapp partner ---> pintu partner

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.oneapp.partner',
-  appName: 'oneapp_partner',
+  appName: 'Pintu Partner',
   webDir: 'www',
   plugins: {
     Keyboard: {
